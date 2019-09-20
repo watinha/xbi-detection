88c8c649d3f2670c0a5af5649b83f4dc4db34d67
Willian Watanabe <watinha@gmail.com>
2019-09-20 16:49:18 -0300

    converting features to float instead of strings

--- pipeline/extractor/xbi_extractor.py
+++ pipeline/extractor/xbi_extractor.py
@@ -14,7 +14,7 @@ class XBIExtractor():
         for feature in self._features:
             Xt.append(arff_data['data'][:,attributes.index(feature)])
 
-        arff_data['X'] = np.array(Xt).T
+        arff_data['X'] = np.array(Xt, dtype='float64').T
         arff_data['y'] = np.array(arff_data['data'][:,attributes.index(self._class_label)])
 
         return arff_data
