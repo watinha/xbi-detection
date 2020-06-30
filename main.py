c96db13fccf2d90d1bb619774fb1720e5219bf1a
Willian Watanabe <watinha@gmail.com>
2020-06-30 15:51:49 -0300

    using feature selection approaches through Cross-Validation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn import tree, svm, ensemble
 from sklearn import metrics
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.neural_network import MLPClassifier
-from sklearn.feature_selection import SelectKBest, chi2, f_classif, RFECV
+from sklearn.feature_selection import SelectKBest, chi2, f_classif, RFE
 from sklearn.model_selection import GridSearchCV,GroupKFold,GroupShuffleSplit,cross_validate
 from functools import reduce
 
@@ -34,6 +34,7 @@ k = int(sys.argv[4])
 extractor_name = sys.argv[1]
 classifier_name = sys.argv[2]
 features = []
+rankings = []
 
 extractor = None
 if extractor_name == 'browserbite':
@@ -96,7 +97,8 @@ if classifier_name == 'randomforest':
             'class_weight': [None, 'balanced']
         }, cv=GroupShuffleSplit(n_splits=3, random_state=42)),
         ensemble.RandomForestClassifier(random_state=42), 'URL')
-    selection_classifier = ensemble.RandomForestClassifier()
+    selection_classifier = tree.DecisionTreeClassifier()
+    #selection_classifier = ensemble.RandomForestClassifier()
 elif classifier_name == 'svm':
     #classifier = ClassifierTunning(GridSearchCV(svm.SVC(), {
     classifier = ClassifierTunning(GridSearchCV(svm.LinearSVC(), {
@@ -133,17 +135,18 @@ elif classifier_name == 'dt':
     selection_classifier = tree.DecisionTreeClassifier()
 else:
     classifier = ClassifierTunning(GridSearchCV(MLPClassifier(), {
-            'hidden_layer_sizes': [5, 10, 30],
+            'hidden_layer_sizes': [10, 20, 30],
             'activation': ['identity', 'logistic', 'tanh', 'relu'],
+            #'activation': ['relu'],
 #            'solver': ['lbfgs', 'sgd', 'adam'],
             'solver': ['adam'],
             'alpha': [0.0001, 0.01, 0.1],
-            'max_iter': [1000],
+            'max_iter': [5000],
             'learning_rate': ['constant', 'invscaling', 'adaptive'],
             'random_state': [42]
         }, cv=GroupShuffleSplit(n_splits=3, random_state=42)),
         MLPClassifier(random_state=42), 'URL')
-    selection_classifier = MLPClassifier()
+    selection_classifier = svm.LinearSVC(dual=False, max_iter=5000)
 
 sampler = TomekLinks()
 
@@ -156,11 +159,24 @@ def cross_val_score_using_sampling(model, X, y, cv, groups, scoring):
     best_precision = []
     best_recall = []
     best_roc = []
+    initial_model = model
     for train_index, test_index in cv.split(X, y, groups):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
+        groups_train = [i for i in groups if groups.index(i) in train_index]
+
+        classifier._grid.fit(X_train, y_train, groups=groups_train)
+        model = initial_model
+        model.set_params(**classifier._grid.best_params_)
+
         X_samp, y_samp = sampler.fit_sample(X_train, y_train)
+        if k < X.shape[1]:
+            X_samp = rfecv.fit_transform(X_samp, y_samp)
+            X_test = rfecv.transform(X_test)
+            rankings.append(rfecv.ranking_)
+
         model.fit(X_samp, y_samp)
+        print('Model trainning with: X (%s)' % (str(X_samp.shape)))
         y_pred = model.predict(X_test)
 
         fscore.append(metrics.f1_score(y_test, y_pred))
@@ -169,16 +185,16 @@ def cross_val_score_using_sampling(model, X, y, cv, groups, scoring):
         roc.append(metrics.roc_auc_score(y_test, y_pred))
 
         try:
-            y_pred = model.predict_proba(X_train)
+            y_pred = model.predict_proba(X_test)
         except:
             model = CalibratedClassifierCV(model)
             model.fit(X_samp, y_samp)
-            y_pred = model.predict_proba(X_train)
+            y_pred = model.predict_proba(X_test)
 
         probability = y_pred[:,1]
 
-        best_roc.append(metrics.roc_auc_score(y_train, probability))
-        precision2, recall2, threasholds = metrics.precision_recall_curve(y_train, probability)
+        best_roc.append(metrics.roc_auc_score(y_test, probability))
+        precision2, recall2, threasholds = metrics.precision_recall_curve(y_test, probability)
         best_f = 0
         best_r = 0
         best_p = 0
@@ -206,17 +222,26 @@ groupcv = None
 groupcv = GroupKFoldCV(GroupShuffleSplit(n_splits=10, random_state=42), 'URL', cross_val_score_using_sampling)
 
 preprocessor = Preprocessor()
-rfecv = RFECV(selection_classifier, cv=3, min_features_to_select=k)
+rfecv = RFE(selection_classifier, n_features_to_select=k)
 #selectkbest = SelectKBest(f_classif, k=k)
 #selector = FeatureSelection(selectkbest, k=k)
 selector = FeatureSelection(rfecv, k=k)
 approach = '%s-%s-%s-k%s' % (extractor_name, classifier_name, class_attr, str(k))
 
 print('running --- %s...' % (approach))
-pipeline = Pipeline([
-    ArffLoader(), XBIExtractor(features, class_attr),
-    extractor, preprocessor, selector, classifier, groupcv
-])
+if extractor_name == 'browserninja2':
+    pipeline = Pipeline([
+        ArffLoader(), XBIExtractor(features, class_attr),
+        BrowserbiteExtractor(class_attr), CrossCheckExtractor(class_attr),
+        #extractor, preprocessor, selector, classifier, groupcv
+        extractor, preprocessor, classifier, groupcv
+    ])
+else:
+    pipeline = Pipeline([
+        ArffLoader(), XBIExtractor(features, class_attr),
+        extractor, preprocessor, classifier, groupcv
+        #extractor, preprocessor, selector, classifier, groupcv
+    ])
 result = pipeline.execute(open('data/07042020/07042020-dataset.binary.hist.img.arff').read())
 print('Model: ' + str(result['model']))
 print('Features: ' + str(result['features']))
@@ -234,35 +259,38 @@ print('Best      Recall: ' + str(result['score']['best_recall']))
 print('Best     ROC: ' + str(result['score']['best_roc']))
 print('Best     ROC: %f' % (reduce(lambda x, y: x+y, result['score']['best_roc']) / 10))
 
-try:
-    sorted_scores = selectkbest.scores_.tolist().copy()
-    sorted_scores.sort()
-    last = None
-    for i in sorted_scores:
-        if (i == last):
-            last = None
-            continue
-        for j in range(len(selectkbest.scores_)):
-            if selectkbest.scores_[j] == i:
-                if len(result['features']) <= j:
-                    print('Feature %d FONT: %f' % (j, i))
-                else:
-                    print('Feature %d %s: %f' % (j, result['features'][j], i))
-        last = i
-except:
-    print('did not run SelectKBest...')
-
-try:
-    print(' --- Features RFECV with %d features --- ' % (rfecv.n_features_))
-    for i in range(len(result['features'])):
-        print('%s -> %d' % (result['features'][i], rfecv.ranking_[i]))
-except:
-    print('did not run RFECV...')
+#try:
+#    sorted_scores = selectkbest.scores_.tolist().copy()
+#    sorted_scores.sort()
+#    last = None
+#    for i in sorted_scores:
+#        if (i == last):
+#            last = None
+#            continue
+#        for j in range(len(selectkbest.scores_)):
+#            if selectkbest.scores_[j] == i:
+#                if len(result['features']) <= j:
+#                    print('Feature %d FONT: %f' % (j, i))
+#                else:
+#                    print('Feature %d %s: %f' % (j, result['features'][j], i))
+#        last = i
+#except:
+#    print('did not run SelectKBest...')
 
 #if k == 3 and (classifier_name == 'dt' or classifier_name == 'randomforest'):
 #    result['model'].fit(result['X'], result['y'])
 #    for i in range(len(result['features'])):
 #        print('%s: %f' % (result['features'][i], result['model'].feature_importances_[i]))
+
+#try:
+#    print(result['features'])
+#    print(' --- Features RFECV with %d features --- ' % (rfecv.n_features_))
+#    for ranking in rankings:
+#        print('--- ranking ---')
+#        for i in range(len(result['features'])):
+#            print('%s -> %d' % (result['features'][i], ranking[i]))
+#except:
+#    print('Did not run RFECV...')
 
 fscore = result['score']['best_f1']
 precision = result['score']['best_precision']
@@ -278,7 +306,19 @@ precision_csv[approach] = precision
 recall_csv[approach] = recall
 roc_csv[approach] = roc
 
+try:
+    features_csv = pd.read_csv('results/features-%s.csv' % (class_attr), index_col=0)
+except:
+    features_csv = pd.DataFrame()
+    features_csv[0] = result['features']
+
+features_len = features_csv.shape[1]
+for i in range(len(rankings)):
+    features_csv['%s-k%d-%d' % (classifier_name, k, (i + features_len))] = rankings[i]
+
+
 fscore_csv.to_csv('results/fscore-%s.csv' % (class_attr))
 precision_csv.to_csv('results/precision-%s.csv' % (class_attr))
 recall_csv.to_csv('results/recall-%s.csv' % (class_attr))
 roc_csv.to_csv('results/roc-%s.csv' % (class_attr))
+features_csv.to_csv('results/features-%s.csv' % (class_attr))
