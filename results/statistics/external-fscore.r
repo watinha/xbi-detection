b3583f50c41048e6183b941406d583e32e0a7d3a
Willian Watanabe <watinha@gmail.com>
2021-11-20 10:42:52 -0300

    includes first version of r scripts for data anaylsis

---
+++ results/statistics/external-fscore.r
@@ -0,0 +1,20 @@
+library(PMCMRplus)
+
+data <- read.csv('../fscore-external.csv')
+
+approaches <- c(
+    'browserbite.dt.external.k300', 'browserbite.randomforest.external.k300', 'browserbite.svm.external.k300', 'browserbite.nn.external.k300',
+    'crosscheck.dt.external.k300', 'crosscheck.randomforest.external.k300', 'crosscheck.svm.external.k300', 'crosscheck.nn.external.k300',
+    'browserninja1.dt.external.k300', 'browserninja1.randomforest.external.k300', 'browserninja1.svm.external.k300', 'browserninja1.nn.external.k300',
+    'browserninja2.dt.external.k300', 'browserninja2.randomforest.external.k300', 'browserninja2.svm.external.k300', 'browserninja2.nn.external.k300' # best RFE result
+)
+
+for (i in approaches) {
+    print(paste('--- ', i, ' ---'))
+    print(shapiro.test(data[,i]))
+}
+
+mat <- data.matrix(data[, approaches])
+friedman.test(mat)
+#posthoc.friedman.nemenyi.test(mat)
+frdAllPairsNemenyiTest(mat)
---
+++ results/statistics/internal-fscore.r
@@ -0,0 +1,20 @@
+library(PMCMRplus)
+
+data <- read.csv('../fscore-internal.csv')
+
+approaches <- c(
+    'browserbite.dt.internal.k300', 'browserbite.randomforest.internal.k300', 'browserbite.svm.internal.k300', 'browserbite.nn.internal.k300',
+    'crosscheck.dt.internal.k300', 'crosscheck.randomforest.internal.k300', 'crosscheck.svm.internal.k300', 'crosscheck.nn.internal.k300',
+    'browserninja1.dt.internal.k300', 'browserninja1.randomforest.internal.k300', 'browserninja1.svm.internal.k300', 'browserninja1.nn.internal.k300',
+    'browserninja2.dt.internal.k300', 'browserninja2.randomforest.internal.k300', 'browserninja2.svm.internal.k300', 'browserninja2.nn.internal.k300' # best RFE result
+)
+
+for (i in approaches) {
+    print(paste('--- ', i, ' ---'))
+    print(shapiro.test(data[,i]))
+}
+
+mat <- data.matrix(data[, approaches])
+friedman.test(mat)
+#posthoc.friedman.nemenyi.test(mat)
+frdAllPairsNemenyiTest(mat)
