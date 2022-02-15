28fb40bea3a76649153ad182cc0e254b3e2ce69f
Willian Watanabe <watinha@gmail.com>
2022-02-15 09:59:19 -0300

    removing unused files

--- pipeline/preprocessing/__init__.py
+++
@@ -1,9 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-
-class Preprocessor ():
-    def execute (self, argument):
-        X = argument['X']
-        scaler = StandardScaler()
-        X_new = scaler.fit_transform(X)
-        argument['X'] = X_new
-        return argument
