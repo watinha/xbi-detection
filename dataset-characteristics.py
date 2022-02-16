7b31015535c77107cebac6ec0dcf10b07c47b0e3
Willian Watanabe <watinha@gmail.com>
2022-02-16 18:18:50 -0300

    build two separate tables, for enhancing the readability of the tables

--- dataset-characteristics.py
+++ dataset-characteristics.py
@@ -16,7 +16,7 @@ def get_alexa_urls():
 
     return [ url[:-1] for url in urls ]
 
-df = load_arff('./data/19112021/dataset.classified.hist.img.arff')
+df = load_arff('./data/19112021/dataset.classified.hist.img.external.arff')
 urls = df['URL'].unique().tolist()
 alexa_urls = get_alexa_urls()
 students, alexa = [], []
@@ -24,28 +24,29 @@ urls_map = {}
 result = []
 
 for url in urls:
+    df['external'] = pd.to_numeric(df['external'], downcast='signed')
+    df['internal'] = pd.to_numeric(df['internal'], downcast='signed')
+    url_data = df.loc[df['URL'] == url].loc[df['targetPlatform'] != 'null'].groupby('targetPlatform')
+    counts, *others = url_data.count()['id'].tolist()
+    internal = url_data.sum()['internal'].tolist()
+    external = url_data.sum()['external'].tolist()
+
     t = url[25:].split('/')[0]
     if (t.isnumeric()):
         urls_map[url] = alexa_urls[int(t)]
+        alexa.append([urls_map[url], counts] + list(sum(zip(external, internal), ())))
     else:
         base = 'http://wwatana.be/mobile/'
         end = url[25:].replace('.notranslate', '')
         urls_map[url] = '%s%s' % (base, end)
+        students.append([urls_map[url], counts] + list(sum(zip(external, internal), ())))
 
-    df['external'] = pd.to_numeric(df['external'], downcast='signed')
-    df['internal'] = pd.to_numeric(df['internal'], downcast='signed')
-    url_data = df.loc[df['URL'] == url].loc[df['targetPlatform'] != 'null'].groupby('targetPlatform')
-    counts = url_data.count()['id'].tolist()
-    internal = url_data.sum()['internal'].tolist()
-    external = url_data.sum()['external'].tolist()
+alexa.sort(key=lambda el: el[0])
+students.sort(key=lambda el: el[0])
 
-    result.append([urls_map[url]] + list(sum(zip(counts, external, internal), ())))
-
-print(result)
-
-result_df = pd.DataFrame(result, columns=['URL',
-    'elements', 'external', 'internal',
-    'elements', 'external', 'internal',
-    'elements', 'external', 'internal'])
-
-print(result_df.to_latex(index=False))
+result_df = pd.DataFrame(students, columns=['URL', 'elements',
+    'external', 'internal', 'external', 'internal', 'external', 'internal'])
+print(result_df.to_latex(index=False, column_format='|l|r|r|r|r|r|r|r|'))
+result_df = pd.DataFrame(alexa, columns=['URL', 'elements',
+    'external', 'internal', 'external', 'internal', 'external', 'internal'])
+print(result_df.to_latex(index=False, column_format='|l|r|r|r|r|r|r|r|'))
