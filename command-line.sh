1f92a380b70475b3892699b97e0d18d7377b882c
Willian Watanabe <watinha@gmail.com>
2022-03-08 19:26:37 -0300

    run all classifiers after extracting features (hopefully, improving the general performance of the script)

--- command-line.sh
+++ command-line.sh
@@ -1,47 +1,23 @@
 echo "===== Browserbite External ====="
 date
-python3 main.py browserbite dt external repeated >> results/browserbite-dt-external.results.txt
-python3 main.py browserbite randomforest external repeated >> results/browserbite-randomforest-external.results.txt
-python3 main.py browserbite svm external repeated >> results/browserbite-svm-external.results.txt
-python3 main.py browserbite nn external repeated >> results/browserbite-nn-external.results.txt
+python3 main.py browserbite external repeated >> results/browserbite-external.results.txt
 echo "===== Browserbite Internal ====="
-python3 main.py browserbite dt internal repeated >> results/browserbite-dt-internal.results.txt
-python3 main.py browserbite randomforest internal repeated >> results/browserbite-randomforest-internal.results.txt
-python3 main.py browserbite svm internal repeated >> results/browserbite-svm-internal.results.txt
-python3 main.py browserbite nn internal repeated >> results/browserbite-nn-internal.results.txt
+python3 main.py browserbite internal repeated >> results/browserbite-internal.results.txt
 
 echo "===== CrossCheck External ====="
 date
-python3 main.py crosscheck dt external repeated >> results/crosscheck-dt-external.results.txt
-python3 main.py crosscheck randomforest external repeated >> results/crosscheck-randomforest-external.results.txt
-python3 main.py crosscheck svm external repeated >> results/crosscheck-svm-external.results.txt
-python3 main.py crosscheck nn external repeated >> results/crosscheck-nn-external.results.txt
+python3 main.py crosscheck external repeated >> results/crosscheck-external.results.txt
 echo "===== CrossCheck Internal ====="
-python3 main.py crosscheck dt internal repeated >> results/crosscheck-dt-internal.results.txt
-python3 main.py crosscheck randomforest internal repeated >> results/crosscheck-randomforest-internal.results.txt
-python3 main.py crosscheck svm internal repeated >> results/crosscheck-svm-internal.results.txt
-python3 main.py crosscheck nn internal repeated >> results/crosscheck-nn-internal.results.txt
+python3 main.py crosscheck internal repeated >> results/crosscheck-internal.results.txt
 
 echo "===== BrowserNinja 1 External ====="
 date
-python3 main.py browserninja1 dt external repeated >> results/browserninja1-dt-external.results.txt
-python3 main.py browserninja1 randomforest external repeated >> results/browserninja1-randomforest-external.results.txt
-python3 main.py browserninja1 svm external repeated >> results/browserninja1-svm-external.results.txt
-python3 main.py browserninja1 nn external repeated >> results/browserninja1-nn-external.results.txt
+python3 main.py browserninja1 external repeated >> results/browserninja1-external.results.txt
 echo "===== BrowserNinja 1 Internal ====="
-python3 main.py browserninja1 dt internal repeated >> results/browserninja1-dt-internal.results.txt
-python3 main.py browserninja1 randomforest internal repeated >> results/browserninja1-randomforest-internal.results.txt
-python3 main.py browserninja1 svm internal repeated >> results/browserninja1-svm-internal.results.txt
-python3 main.py browserninja1 nn internal repeated >> results/browserninja1-nn-internal.results.txt
+python3 main.py browserninja1 internal repeated >> results/browserninja1-internal.results.txt
 
 echo "===== BrowserNinja 2 External ====="
 date
-python3 main.py browserninja2 dt external repeated >> results/browserninja2-dt-external.results.txt
-python3 main.py browserninja2 randomforest external repeated >> results/browserninja2-randomforest-external.results.txt
-python3 main.py browserninja2 svm external repeated >> results/browserninja2-svm-external.results.txt
-python3 main.py browserninja2 nn external repeated >> results/browserninja2-nn-external.results.txt
+python3 main.py browserninja2 external repeated >> results/browserninja2-external.results.txt
 echo "===== BrowserNinja 2 Internal ====="
-python3 main.py browserninja2 dt internal repeated >> results/browserninja2-dt-internal.results.txt
-python3 main.py browserninja2 randomforest internal repeated >> results/browserninja2-randomforest-internal.results.txt
-python3 main.py browserninja2 svm internal repeated >> results/browserninja2-svm-internal.results.txt
-python3 main.py browserninja2 nn internal repeated >> results/browserninja2-nn-internal.results.txt
+python3 main.py browserninja2 internal repeated >> results/browserninja2-internal.results.txt
--- main.py
+++ main.py
@@ -12,14 +12,12 @@ from pipeline.loader.arff_loader import ArffLoader
 
 random.seed(42)
 
-def main(extractor_name, classifier_name, class_attr, sampler_name, n_splits, path='.'):
+def main(extractor_name, class_attr, sampler_name, n_splits, path='.'):
     (extractor, features, nfeatures, max_features) = get_extractor(
             extractor_name, class_attr)
-    gridsearch = get_classifier(classifier_name, nfeatures, max_features)
     sampler = get_sampler(sampler_name)
 
-    approach = '%s-%s-%s' % (extractor_name, classifier_name, class_attr)
-    print('running --- %s...' % (approach))
+    print('running --- %s-%s...' % (extractor_name, class_attr))
 
     extractor_pipeline = Pipeline([
         ArffLoader(), XBIExtractor(features, class_attr), extractor ])
@@ -31,112 +29,118 @@ def main(extractor_name, classifier_name, class_attr, sampler_name, n_splits, pa
     print('data extracted...')
     rankings, fscore, precision, recall, roc, train_fscore = [], [], [], [], [], []
     cv = GroupShuffleSplit(n_splits=n_splits, random_state=42)
-    for train_index, test_index in cv.split(X, y, groups):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        groups_train = [i for i in groups if groups.index(i) in train_index]
 
-        X_samp, y_samp = sampler.fit_resample(X_train, y_train)
-        groups_samp = [
-                groups_train[X_train.tolist().index(row)] for row in X_samp.tolist() ]
+    for classifier_name in ['svm', 'nn', 'dt', 'randomforest']:
+        approach = '%s-%s-%s' % (extractor_name, classifier_name, class_attr)
+        print('running --- %s...' % (approach))
+        gridsearch = get_classifier(classifier_name, nfeatures, max_features)
 
-        print('Model trainning with: X (%s)' % (str(X_samp.shape)))
-        gridsearch.fit(X_samp, y_samp, groups=groups_samp)
-        print('Model trained with fscore %s, and params %s ' % (str(gridsearch.best_score_), str(gridsearch.best_params_)))
+        for train_index, test_index in cv.split(X, y, groups):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            groups_train = [i for i in groups if groups.index(i) in train_index]
 
-        selector = gridsearch.best_estimator_.named_steps['selector']
-        rankings.append(selector.get_support(indices=False))
+            X_samp, y_samp = sampler.fit_resample(X_train, y_train)
+            groups_samp = [
+                    groups_train[X_train.tolist().index(row)] for row in X_samp.tolist() ]
 
-        if (classifier_name == 'svm'):
-            model = CalibratedClassifierCV(gridsearch.best_estimator_)
-            model.fit(X_samp, y_samp)
-        else:
-            model = gridsearch
+            print('Model trainning with: X (%s)' % (str(X_samp.shape)))
+            gridsearch.fit(X_samp, y_samp, groups=groups_samp)
+            print('Model trained with fscore %s, and params %s ' %
+                    (str(gridsearch.best_score_), str(gridsearch.best_params_)))
 
-        y_pred = model.predict_proba(X_train)
-        probability = y_pred[:, list(
-            gridsearch.best_estimator_.named_steps['classifier'].classes_).index(1)]
+            selector = gridsearch.best_estimator_.named_steps['selector']
+            rankings.append(selector.get_support(indices=False))
 
-        precision2, recall2, threasholds = metrics.precision_recall_curve(
-                y_train, probability)
-        best_f = 0
-        threashold = 0
-        for i in range(len(precision2)):
-            new_fscore = (2 * precision2[i] * recall2[i]) / (precision2[i] + recall2[i])
-            if new_fscore > best_f:
-                best_f = new_fscore
-                threshold = threasholds[i]
+            if (classifier_name == 'svm'):
+                model = CalibratedClassifierCV(gridsearch.best_estimator_)
+                model.fit(X_samp, y_samp)
+            else:
+                model = gridsearch
 
-        print('Model training F-Score with selected threshold: %f' % (metrics.f1_score(y_train, [ 0 if prob < threshold else 1 for prob in probability])))
+            y_pred = model.predict_proba(X_train)
+            probability = y_pred[:, list(
+                gridsearch.best_estimator_.named_steps['classifier'].classes_).index(1)]
 
-        train_fscore.append(metrics.f1_score(y_train, [ 0 if prob < threshold else 1 for prob in probability]))
-        y_pred = model.predict_proba(X_test)
-        probability = y_pred[:, list(
-            gridsearch.best_estimator_.named_steps['classifier'].classes_).index(1)]
+            precision2, recall2, threasholds = metrics.precision_recall_curve(
+                    y_train, probability)
+            best_f = 0
+            threashold = 0
+            for i in range(len(precision2)):
+                new_fscore = (2 * precision2[i] * recall2[i]) / (precision2[i] + recall2[i])
+                if new_fscore > best_f:
+                    best_f = new_fscore
+                    threshold = threasholds[i]
 
-        y_threashold = [ 0 if y < threshold else 1 for y in probability]
-        print('Model tested with F-Score: %f' % (metrics.f1_score(y_test, y_threashold)))
-        fscore.append(metrics.f1_score(y_test, y_threashold))
-        precision.append(metrics.precision_score(y_test, y_threashold))
-        recall.append(metrics.recall_score(y_test, y_threashold))
-        roc.append(metrics.roc_auc_score(y_test, y_threashold))
+            print('Model training F-Score with selected threshold: %f' % (metrics.f1_score(y_train, [ 0 if prob < threshold else 1 for prob in probability])))
+
+            train_fscore.append(metrics.f1_score(y_train, [ 0 if prob < threshold else 1 for prob in probability]))
+            y_pred = model.predict_proba(X_test)
+            probability = y_pred[:, list(
+                gridsearch.best_estimator_.named_steps['classifier'].classes_).index(1)]
+
+            y_threashold = [ 0 if y < threshold else 1 for y in probability]
+            print('Model tested with F-Score: %f' % (metrics.f1_score(y_test, y_threashold)))
+            fscore.append(metrics.f1_score(y_test, y_threashold))
+            precision.append(metrics.precision_score(y_test, y_threashold))
+            recall.append(metrics.recall_score(y_test, y_threashold))
+            roc.append(metrics.roc_auc_score(y_test, y_threashold))
 
 
-    print('Features: ' + str(features))
-    print('X dimensions:' + str(X.shape))
-    print('Test     ROC: %f' % (reduce(lambda x,y: x+y, roc) / n_splits))
-    print('Test      F1: %f' % (reduce(lambda x,y: x+y, fscore) / n_splits))
-    print('Test      F1: ' + str(fscore))
-    print('Train     F1: ' + str(train_fscore))
-    print('Test      Precision: ' + str(precision))
-    print('Test      Recall: ' + str(recall))
+        print('Features: ' + str(features))
+        print('X dimensions:' + str(X.shape))
+        print('Test     ROC: %f' % (reduce(lambda x,y: x+y, roc) / n_splits))
+        print('Test      F1: %f' % (reduce(lambda x,y: x+y, fscore) / n_splits))
+        print('Test      F1: ' + str(fscore))
+        print('Train     F1: ' + str(train_fscore))
+        print('Test      Precision: ' + str(precision))
+        print('Test      Recall: ' + str(recall))
 
-    try:
-        fscore_csv = pd.read_csv('results/fscore-%s.csv' % (class_attr), index_col=0)
-        precision_csv = pd.read_csv(
-                'results/precision-%s.csv' % (class_attr), index_col=0)
-        recall_csv = pd.read_csv('results/recall-%s.csv' % (class_attr), index_col=0)
-        roc_csv = pd.read_csv('results/roc-%s.csv' % (class_attr), index_col=0)
-    except:
-        fscore_csv = pd.DataFrame()
-        precision_csv = pd.DataFrame()
-        recall_csv = pd.DataFrame()
-        roc_csv = pd.DataFrame()
+        try:
+            fscore_csv = pd.read_csv('results/fscore-%s.csv' % (class_attr), index_col=0)
+            precision_csv = pd.read_csv(
+                    'results/precision-%s.csv' % (class_attr), index_col=0)
+            recall_csv = pd.read_csv('results/recall-%s.csv' % (class_attr), index_col=0)
+            roc_csv = pd.read_csv('results/roc-%s.csv' % (class_attr), index_col=0)
+        except:
+            fscore_csv = pd.DataFrame()
+            precision_csv = pd.DataFrame()
+            recall_csv = pd.DataFrame()
+            roc_csv = pd.DataFrame()
 
-    fscore_csv.loc[:, approach] = fscore
-    precision_csv.loc[:, approach] = precision
-    recall_csv.loc[:, approach] = recall
-    roc_csv.loc[:, approach] = roc
+        fscore_csv.loc[:, approach] = fscore
+        precision_csv.loc[:, approach] = precision
+        recall_csv.loc[:, approach] = recall
+        roc_csv.loc[:, approach] = roc
 
-    fscore_csv.to_csv('results/fscore-%s.csv' % (class_attr))
-    precision_csv.to_csv('results/precision-%s.csv' % (class_attr))
-    recall_csv.to_csv('results/recall-%s.csv' % (class_attr))
-    roc_csv.to_csv('results/roc-%s.csv' % (class_attr))
+        fscore_csv.to_csv('results/fscore-%s.csv' % (class_attr))
+        precision_csv.to_csv('results/precision-%s.csv' % (class_attr))
+        recall_csv.to_csv('results/recall-%s.csv' % (class_attr))
+        roc_csv.to_csv('results/roc-%s.csv' % (class_attr))
 
-    try:
-        features_csv = pd.read_csv('results/features-%s.csv' % (class_attr), index_col=0)
-    except:
-        features_csv = pd.DataFrame(columns=features)
+        try:
+            features_csv = pd.read_csv('results/features-%s.csv' % (class_attr), index_col=0)
+        except:
+            features_csv = pd.DataFrame(columns=features)
 
-    features_len = features_csv.shape[1]
-    print(features)
-    print(features_len)
-    print(rankings)
+        features_len = features_csv.shape[1]
+        print(features)
+        print(features_len)
+        print(rankings)
 
-    if extractor_name == 'browserninja2':
-        for i in range(len(rankings)):
-            features_csv.loc[
-                    '%s-%d' % (classifier_name, (i + features_len)), :] = rankings[i]
-        features_csv.to_csv('results/features-%s.csv' % (class_attr))
+        if extractor_name == 'browserninja2':
+            for i in range(len(rankings)):
+                features_csv.loc[
+                        '%s-%d' % (classifier_name, (i + features_len)), :] = rankings[i]
+            features_csv.to_csv('results/features-%s.csv' % (class_attr))
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 5, 'The script requires 4 parameters: feature extractor (browserbite|crosscheck|browserninja1|browserninja2), classifier (randomforest|svm|dt|nn), type of xbi (internal|external) and sampler strategy (none|tomek|near|repeated|rule|random)'
+    assert len(sys.argv) == 4, 'The script requires 3 parameters: feature extractor (browserbite|crosscheck|browserninja1|browserninja2), type of xbi (internal|external) and sampler strategy (none|tomek|near|repeated|rule|random)'
 
-    class_attr = sys.argv[3]
     extractor_name = sys.argv[1]
-    classifier_name = sys.argv[2]
-    sampler_name = sys.argv[4]
+    class_attr = sys.argv[2]
+    sampler_name = sys.argv[3]
     n_splits = 24
 
-    main(extractor_name, classifier_name, class_attr, sampler_name, n_splits)
+    main(extractor_name, class_attr, sampler_name, n_splits)
